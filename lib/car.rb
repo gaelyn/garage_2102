8fb8653a89053e874b7daf9933399341a5782277
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-25 11:29:56 -0600

    Add car class and tests

---
+++ lib/car.rb
@@ -0,0 +1,15 @@
+class Car
+  attr_reader :make,
+              :model,
+              :color,
+              :year,
+              :age
+  def initialize(info)
+    split_string = info[:description].split
+    @make = split_string[1]
+    @model = split_string[2]
+    @color = split_string[0]
+    @year = info[:year]
+    @age = 2021 - (info[:year].to_i)
+  end
+end
